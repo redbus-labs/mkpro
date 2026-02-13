38f4153840fd4b76a3de573e229b6fde59cd8c87
manoj.kumar <manoj.kumar@redbus.com>
2026-02-13 18:56:56 +0530

    chore: Cleanup additional files

--- inject.ps1
+++
@@ -1,8 +0,0 @@
-$file = "src/main/java/com/mkpro/Maker.java"
-$content = Get-Content $file -Raw
-if (-not $content.Contains("public static String backItUp(File file)")) {
-    Write-Host "Not found"
-} else {
-    Write-Host "Found"
-}
-$file = "src/main/java/com/mkpro/Maker.java"
